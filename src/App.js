2f3de1acd46b8b550a4e71d1f38d741ef6b6d38a
Timothy Sukamtoh <timtoh168@gmail.com>
2023-06-01 09:47:21 -0700

    include comments on how to handle user

--- src/App.js
+++ src/App.js
@@ -7,16 +7,26 @@ import JoblyApi from './api';
 import NavBar from './NavBar';
 import RouteList from './RouteList';
 import userContext from "./userContext";
-
+/**TODO:
+ * 1. form submitted
+ * 2. send to backend
+ * 3. receive a token string
+ * 4. set state with token string
+ * 5. once set of state has finished, useEffect runs if token changed
+ * 6. in use effect, decode token to get username
+ * 7. use username to make a request to backend
+ * 8. gives an object back with that user
+ * 9. set current state for user
+ */
 /**Component for App */
 function App() {
   const [currentUser, setCurrentUser] = useState({});
   const [token, setToken] = useState("");
 
   /**
-     * Makes API post request to login
-     * @param {Object} formData data from form
-     */
+   * Makes API post request to login
+   * @param {Object} formData data from form
+   */
   async function login(formData) {
     const token = await JoblyApi.login(formData);
     setToken(token);
@@ -25,9 +35,9 @@ function App() {
   }
 
   /**
-    * Makes API post request to sign up
-    * @param {Object} formData data from form
-    */
+   * Makes API post request to sign up
+   * @param {Object} formData data from form
+   */
   async function signUp(formData) {
     const token = await JoblyApi.signUp(formData);
     setToken(token);
