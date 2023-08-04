fc1e023db6c7c9b58b76883e1de2b73588ed2564
Timothy Sukamtoh <timtoh168@gmail.com>
2023-08-04 13:49:37 -0700

    changing navbar

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/dist/js/bootstrap.bundle.min";
+import "bootstrap/dist/js/bootstrap.js";
 import './custom.scss';
 
 import './index.css';
