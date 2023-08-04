68484839f54fa9a7f7ccbf6b3f04b92ebb23113f
Timothy Sukamtoh <timtoh168@gmail.com>
2023-08-04 16:14:59 -0700

    uniform details page

--- src/CompanyDetailsPage.js
+++ src/CompanyDetailsPage.js
@@ -27,9 +27,9 @@ function CompanyDetailsPage() {
   if (!company) return <h1>Loading...</h1>;
 
   return(
-    <div className="text-start">
-      <h2 className = "ms-5 mt-3">{company.name}</h2>
-      <h4  className ="ms-5">{company.description}</h4>
+    <div className="bg-primary pt-5 text-black bg-opacity-25 bg-gradient">
+      <h2 className="ms-5 mt-3">{company.name}</h2>
+      <h4  className="ms-5">{company.description}</h4>
       <JobList jobs={company.jobs} />
     </div>
   )
